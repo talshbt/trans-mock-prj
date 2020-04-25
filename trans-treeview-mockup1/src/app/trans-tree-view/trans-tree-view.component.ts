b9d0b88fc9d9f2faba723f87f24c627e374336f2
talshbt <talshbt@gmail.com>
2020-04-25 11:20:27 +0300

    createTreeMainDs

--- trans-treeview-mockup1/src/app/trans-tree-view/trans-tree-view.component.ts
+++ trans-treeview-mockup1/src/app/trans-tree-view/trans-tree-view.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { RestApiService } from "../rest-api.service";
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { TransService } from "../shared/trans.service";
 import { TransDetails } from "../shared/trans-details.model";
+import { NgForm, FormBuilder, FormGroup, FormArray, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-trans-tree-view',
@@ -12,33 +13,33 @@ import { TransDetails } from "../shared/trans-details.model";
 })
 export class TransTreeViewComponent implements OnInit, OnDestroy {
   subscription: Subscription;
+  // myForm: FormGroup;
+  public transForm ;
+  form;
 
   title = 'an-mockup-trans0';
   count = 0;
-  arr = {};
-  //  transName = 'luAnc1ServerData';
+  transTree = {};
   transName = null;
-
+ 
   tatSherutName = null;
-
-  constructor(public restApi: RestApiService, private transService: TransService) {
-
+  // store the data for form
+  treeMainDs = [];
+  constructor( public restApi: RestApiService, private transService: TransService) {
+  
   }
-
   ngOnInit() {
     this.subscription = this.transService.detailsChanged.subscribe(
       data => {
         this.transName = data.transName;
         this.tatSherutName = data.tatSherutName;
 
-
-        // this.restApi.getArr('luAnc1ServerData', null).subscribe((data: {}) => {
-        //   this.arr = data;
-        // })
-
-
         this.transService.getTreeEvent.subscribe(tree=>{
-          this.arr = tree;
+          this.transTree = tree;
+          this.createTreeMainDs();
+          console.log(this.treeMainDs)
+          this.setFormArray()
+    
         })
 
 
@@ -46,19 +47,20 @@ export class TransTreeViewComponent implements OnInit, OnDestroy {
     );
 
 
-    // console.log(this.transName)
-
 
 
 
     this.transService.getTreeEvent.subscribe(tree=>{
-      this.arr = tree;
+      this.transTree = tree;
     })
 
 
 
   }
 
+  onSubmit() {
+    // alert(this.myForm.value);
+  }
   getHttpReq() {
 
   }
@@ -69,18 +71,101 @@ export class TransTreeViewComponent implements OnInit, OnDestroy {
 
 
 
+  // setParents(){
+  //   let control = <FormArray>this.myForm.controls.cities;
+  //   // this.data.cities.forEach(x => {
+  //   //   control.push(this.fb.group({ 
+  //   //     city: x.city, 
+  //   //     addressLines: this.setAddressLines(x) }))
+  //   // })
+  // }
+  hasChildren(parent){
+    return parent.length > 0
+  }
 
+
+
+  createObj(parent){
+       var group = { };
+
+     for (var i in this.transTree[parent]){
+              
+             group[this.transTree[parent][i]] = new FormControl('', {validators: Validators.required});
+    
+            }
+
+
+            return group;
+  }
+
+  setFormArray(){
+    let storeGroup = [];
+
+              for (var p in this.transTree) {
+    
+             if(this.hasChildren(this.transTree[p])){
+
+
+               let group  = this.createObj(p)
+
+
+                storeGroup.push(new FormGroup(group))
+
+       
+             }
+
+             this.transForm = new FormArray(storeGroup);
+
+          
+             
+  }
+
+
+  
+  
+  
+}
+
+createTreeMainDs() {
+  var group = {};
+  var counter = 0;
+  for (var parent in this.transTree) {
+    var formArr = [];
+    var parentObj = {};
+    parentObj["name"] = parent;
+    if (!this.hasChild(parent)) {
+      formArr.push(new FormControl(parent));
+      parentObj["hasChild"] = false;
+    } else {
+      var children = [];
+      var childObj = {};
+      parentObj["hasChild"] = true;
+      for (var child in this.transTree[parent]) {
+        formArr.push(new FormControl(this.transTree[parent][child]));
+        childObj[this.transTree[parent][child]] = this.transTree[parent][
+          child
+        ];
+        children.push(this.transTree[parent][child]);
+      }
+      parentObj["children"] = children;
+      parentObj["childObj"] = childObj;
+    }
+
+    this.treeMainDs.push(parentObj);
+  }
+ 
 
 }
 
 
 
+hasChild(parent) {
+  return this.transTree[parent].length > 0;
+}
 
 
 
-
-
-
+}
 
 
 
