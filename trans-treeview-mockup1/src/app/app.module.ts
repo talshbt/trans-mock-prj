eea27a6a52c64f8eada561a053b3219fc9614860
talshbt <talshbt@gmail.com>
2020-04-26 14:23:19 +0300

    add table

--- trans-treeview-mockup1/src/app/app.module.ts
+++ trans-treeview-mockup1/src/app/app.module.ts
@@ -9,21 +9,36 @@ import { AppComponent } from './app.component';
 import { TransInputComponent } from './trans-input/trans-input.component';
 import { TransTreeViewComponent } from './trans-tree-view/trans-tree-view.component';
 import { AppService } from './shared/app.service';
+import { TableTransMockupComponent } from './table-trans-mockup/table-trans-mockup.component';
+import { TableComponent } from './table-trans-mockup/table/table.component';
+import { RowComponent } from './table-trans-mockup/table/row/row.component';
+import { AddItemComponent } from './table-trans-mockup/add-item/add-item.component';
+import { NgModalComponent } from './table-trans-mockup/ng-modal/ng-modal.component';
+import { FilterPipe } from './shared/filter.pipe';
+import { NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
     AppComponent,
     TransInputComponent,
-    TransTreeViewComponent
+    TransTreeViewComponent,
+    TableTransMockupComponent,
+    TableComponent,
+    RowComponent,
+    AddItemComponent,
+    NgModalComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgbModalModule,
     ReactiveFormsModule,
     FormsModule
   ],
-  entryComponents: [TransTreeViewComponent],
+  entryComponents: [TransTreeViewComponent,NgModalComponent, AddItemComponent, TableComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
